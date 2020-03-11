82b105b834a1af45a4e683552704d670407a01ba
Prajjwal <pdangal@uccs.edu>
2020-03-11 16:56:47 -0600

    committing what I have so far, first commit

---
+++ kmain.c
@@ -0,0 +1,6 @@
+// linking C, 32-bit addresses
+struct example {
+        unsigned char config;   /* bit 0 - 7   */
+        unsigned short address; /* bit 8 - 23  */
+        unsigned char index;    /* bit 24 - 31 */
+    } __attribute__((packed));
