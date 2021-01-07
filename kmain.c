5e4f405aa43604f5ee95154c0ac6d35b6786bf07
prajjwaldangal <pdangal@uccs.edu>
2021-01-06 21:26:19 -0700

    hello world working

--- kmain.c
+++ kmain.c
@@ -1,6 +1,24 @@
- /* The C function */
-    int sum_of_three(int arg1, int arg2, int arg3)
-    {
-        return arg1 + arg2 + arg3;
-    }
+#define FB_GREEN     2
+#define FB_DARK_GREY 8
 
+/** fb_write_cell:
+ *  Writes a character with the given foreground and background to position i
+ *  in the framebuffer.
+ *
+ *  @param i  The location in the framebuffer
+ *  @param c  The character
+ *  @param fg The foreground color
+ *  @param bg The background color
+ */
+void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
+{
+    fb[i] = c;
+    fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
+}
+
+/* The C function */
+int _hook()
+{
+    fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);
+    return 0;
+}
